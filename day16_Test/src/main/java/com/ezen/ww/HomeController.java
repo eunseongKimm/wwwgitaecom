77df72b4d322bfff409cea0436e9da72bee60bdf
HeonSu Mok <mhs970818@naver.com>
2023-09-26 15:35:27 +0900

    문제2번 쇼핑몰 입력입니다

---
+++ day16_Test/src/main/java/com/ezen/ww/HomeController.java
@@ -0,0 +1,28 @@
+package com.ezen.ww;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController 
+{
+	@RequestMapping(value="/")
+	public String main()
+	{
+		return "main";
+	}
+	
+	@RequestMapping(value="/main")
+	public String main1()
+	{
+		return "main";
+	}
+}
